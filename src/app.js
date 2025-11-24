5c3e2aece2089caa4ab7610eb1a4bd2f5471ddb2
ManuelFMG <manuel_psp86@hotmail.com>
2025-11-24 19:30:58 +0000

    IntentoConDistintasExtensiones

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-import "bootstrap";
-import "./style.css";
 
+//  Posición     0      1
+let pronoun = ['the', 'our'];
+//  Posición  0       1
+let adj = ['great', 'big'];
+//  Posición   0          1
+let noun = ['jogger', 'racoon'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Bucle Loop Anidado
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Primer arreglo pronun (indice = i)
+for (let i = 0; i < pronoun.length; i++) {
+  // Segundo arreglo adj (indice = i2)
+  for (let i2 = 0; i2 < adj.length; i2++) {
+    // Tercer arreglo noun (indice = i3)
+    for (let i3 = 0; i3 < noun.length; i3++) {
+     
+      // Impresión con distintos tipos de extensiones: .com/.net/.us/.io
+      
+       // Impresión 1
+       console.log(pronoun[i]+adj[i2]+noun[i3]+".com");
+       
+       // Impresión 2
+       console.log(pronoun[i]+adj[i2]+noun[i3]+".net");
+       
+       // Impresión 3
+       console.log(pronoun[i]+adj[i2]+noun[i3]+".us");
+       
+       // Impresión 4
+       console.log(pronoun[i]+adj[i2]+noun[i3]+".io");
+       
+       
+    }
+
+  }
+
+}
+
